b7d3c1cc8e75fca2114b54eb900f1a7ce26f6f28
Arif Rachmat Darmawan <arifrd200211@gmail.com>
2023-05-26 17:34:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/utspraktik_if6_10120211_arifrachmatdarmawan/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.utspraktik_if6_10120211_arifrachmatdarmawan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+//    26 Mei 2023
+//    10120211 - Arif Rachmat Darmawan - IF6
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
